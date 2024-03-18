fec745a7c293ce4e9c67bc4ae7c1862ca1b4bdf8
Gantavya Saraswat <gantavya99@gmail.com>
2024-03-18 23:40:30 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./App.css";
+function App() {
+  return <>
+  <h1>
+    TodoMatic
+  </h1>
+  <h2>
+    What needs to be done?
+  </h2>
+  </>;
+}
+
+export default App;
+
+// Create a Todo app using React.js. Refer to the given image in the attachment.
+// You can use your own creativity for the look and feel.
+// Must have the functionality to Create, Edit, and Delete Tasks and filter them by Active/Completed/All.
+// Guideline:
+
+// Must have used one functional component with Hooks and one class component with Life cycle methods.
+// Must have used Redux for global state management.
+// Bonus points for organized folder structure.
+// Bonus points for persisting the data on reload.
+// Bonus points for using Next.js.
+// Bonus points for mobile responsiveness.
